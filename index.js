d2e44b88f618fa9622c2873b48e3390e4ab50b12
Aryan Dutt <aryandutt21@gmail.com>
2021-07-11 17:16:19 +0530

    add subtract

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function clickMe(){
     btn.innerHTML = num;
     console.log(num);
 }
+function subtract(){
+    var butt = document.querySelector('#hello h1');
+    var val = butt.innerHTML;
+    val = parseInt(val);
+    if(val==0){
+        return;
+    }
+    val = val - 1;
+    butt.innerHTML = val;
+    // console.log(val);
+}
