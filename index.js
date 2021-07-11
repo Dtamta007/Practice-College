1f96ba23044cde3187b30fa0a32b33288ffbf378
gcc-deepanshu <deepanshu.glasscode@gmail.com>
2021-07-11 16:52:18 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("hello bois");
+
+function clickMe(){
+    var btn = document.querySelector('#hello h1');
+    var val = btn.innerHTML;
+    console.log("hello");
+    console.log(btn);
+    var num = parseInt(val)
+    num = num+1;
+    btn.innerHTML = num;
+    console.log(num);
+}
